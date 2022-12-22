5d17cbda53d9dca73180d6ea206aa4fad533230b
Breymunt <serhei.dobrovolskiy@ukr.net>
2022-12-22 11:52:18 +0200

    added stock rack in FM

--- src/functions.js
+++ src/functions.js
@@ -36,6 +36,34 @@ export function calcWork(width, length) {
 }
 
 //Фундамент монолітний Фм1
+function getStock(width) {
+  if (width == 16) {
+    return {
+      s: 8,
+    };
+  } else if (width == 18) {
+    return {
+      s: 8,
+    };
+  } else if (width == 20) {
+    return {
+      s: 8,
+    };
+  } else if (width == 22) {
+    return {
+      s: 8,
+    };
+  } else if (width == 24) {
+    return {
+      s: 8,
+    };
+  } else if (width == 30) {
+    return {
+      s: 8,
+    };
+  }
+}
+
 function getFm1Consts(width) {
   if (width <= 20) {
     return {
@@ -61,11 +89,12 @@ function getFm1Consts(width) {
 export function calcFm1(width, length) {
   const count = length / 3 + 2;
   const consts = getFm1Consts(width);
+  const stock = getStock(width);
 
-  const arm12Amount = Math.ceil((count * consts.arm + 84) / 12) * 12;
+  const arm12Amount =
+    Math.ceil((count * consts.arm + stock.s * 8 + 84) / 12) * 12;
   const ankerAmount = Math.ceil(count * consts.anker);
-  const betonAmount = Math.ceil(count * consts.beton);
-
+  const betonAmount = Math.ceil(count * consts.beton + stock.s * 0.13);
   const arm12Price = arm[12] * arm12Amount;
   const ankerPrice = anker[24] * ankerAmount;
   const betonPrice = beton * betonAmount;
